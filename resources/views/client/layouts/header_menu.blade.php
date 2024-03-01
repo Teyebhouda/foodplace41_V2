3c838280de62d694bc50f214825ff7a287d4dcf6
HoudaTeyeb <teyebhouda1994@gmail.com>
2024-03-01 15:55:11 +0100

    tva config

--- resources/views/client/layouts/header_menu.blade.php
+++ resources/views/client/layouts/header_menu.blade.php
@@ -100,11 +100,11 @@
 </strong></span>
            <a class="btn-book-a-table" href="{{ url('/edit-profile') }}" style="margin-right:15px;color: white;font-size: medium">Modifier Compte </a>
            <a class="btn-book-a-table" href="{{ url('/logout') }}" style="margin-right:15px;color: white;font-size: medium">Déconnexion </a>
-          
+<!--           
            <form action="{{ route('client.logout') }}" method="POST">
             @csrf
             <button type="submit" class="btn-book-a-table">Déconnexion</button>
-        </form>
+        </form> -->
 			  
 			
 
